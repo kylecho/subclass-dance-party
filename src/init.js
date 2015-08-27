dd138f7b83d1c53619d5f65ac83de1812a9ca4f4
kylecho <kylecho.work@gmail.com>
2015-08-27 10:08:49 -0700

    Add variety of dancers

--- src/init.js
+++ src/init.js
@@ -1,38 +1,15 @@
 $(document).ready(function(){
   window.dancers = [];
-  window.gifs = [];
+  window.gifs = ['banana','granma','man','women','stick','pinkbear','rainbow','rat'];
 
   $(".addDancerButton").on("click", function(event){
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
-    // var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
-    // get the maker function for the kind of dancer we're supposed to make
-    // var dancerMakerFunction = window[dancerMakerFunctionName];
-
-    // make a dancer with a random position
-
     var dancer = new makeBlinkyDancer(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-
     window.dancers.push(dancer);
-
     $('body').append(dancer.$node);
-
   });
 
   $('.addSquareDancerButton').on("click", function(){
@@ -41,9 +18,7 @@ $(document).ready(function(){
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-
     window.dancers.push(squareDancer);
-
     $('body').append(squareDancer.$node);
     $(squareDancer.$node).prepend('<img src="img/banana.gif" />');
   });
@@ -54,19 +29,27 @@ $(document).ready(function(){
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-
     window.dancers.push(bigDancer);
-
     $('body').append(bigDancer.$node);
   });
 
+  // line dancers up
   $('.lineUpDancerButton').on("click", function(){
     for (var i = 0; i < window.dancers.length; i++) {
       window.dancers[i].lineUp($("body").height()/2);
     }
+  });  
+
+  $('.randomDancerButton').on("click", function(){
+    var randomDancer = new makeDancer(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(randomDancer);
+    $('body').append(randomDancer.$node);
+    $(randomDancer.$node).prepend('<img src="img/' + window.gifs[Math.floor(Math.random() * window.gifs.length)] + '.gif" />');
   });
-
-
 
 });
 
